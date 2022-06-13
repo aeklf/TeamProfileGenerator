f796a1278b76fbd98fd9e7df3d834b10cc3dfdc4
Andrea Kalife <andreaklfdlg@gmail.com>
2022-06-13 17:04:47 -0500

    Final JS

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const path = require ("path");
 
 const fs = require("fs");
 
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "index.html");
+
+const render =("./lib/htmlRenderer");
 const {run} = require("jest");
 
 const teamProfile = [];
